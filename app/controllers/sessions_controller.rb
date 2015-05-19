79d9140b45d856ec5e10f41d976ac24b828b5aae
Sung Kim <sunggyun187@gmail.com>
2015-05-19 12:59:32 -0400

    changed window.load to doc.ready for redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,5 +23,5 @@ class SessionsController < ApplicationController
         session[:user_id] = nil
         flash[:notice] = "You have been logged out!"
         redirect_to root_path
-    end 
+    end
 end
